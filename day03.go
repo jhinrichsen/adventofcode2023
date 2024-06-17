6ea786308b9a7933716d41b6818e8a2851a57697
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2024-06-17 11:38:11 +0200

    feat: day 3, part 2

--- day03.go
+++ day03.go
@@ -6,7 +6,7 @@ import (
 	"github.com/fatih/color"
 )
 
-func Day03(lines []string) (sum int) {
+func Day03Part1(lines []string) (sum int) {
 	isSurrounded := func(x, y, l int) bool {
 		x1 := max(x-l-1, 0)
 		x2 := min(x, len(lines[y])-1)
@@ -74,6 +74,7 @@ func Day03(lines []string) (sum int) {
 	}
 	return
 }
+
 func Day03ColoredLogging(lines []string) (sum int) {
 	isSurrounded := func(x, y, l int) bool {
 		x1 := max(x-l-1, 0)
@@ -107,7 +108,7 @@ func Day03ColoredLogging(lines []string) (sum int) {
 	// events for digits and numbers
 
 	var n, l int
-	enter := func(x, y int) {
+	enter := func() {
 		n, l = 0, 0
 		// color.Set(color.FgYellow)
 	}
@@ -133,7 +134,7 @@ func Day03ColoredLogging(lines []string) (sum int) {
 			if isDigit(c) {
 				high = true
 				if high != last {
-					enter(x, y)
+					enter()
 				}
 				last = high
 				digit(c)
@@ -156,10 +157,121 @@ func Day03ColoredLogging(lines []string) (sum int) {
 	return
 }
 
+func Day03Part2(lines []string) (sum uint) {
+	const maxNeighbours = 6
+
+	var numbers [maxNeighbours]uint // avoid heap allocs
+	var n byte                      // index into numbers
+
+	maxX := len(lines[0]) - 1
+	maxY := len(lines) - 1
+
+	// 'A gear is any * symbol that is adjacent to exactly two part numbers.'
+	isGear := func() bool {
+		return n == 2
+	}
+
+	gearRatio := func() uint {
+		if !isGear() {
+			return 0
+		}
+		return numbers[0] * numbers[1]
+	}
+
+	left := func(x, y int) int {
+		for x > 0 && isDigit(lines[y][x]) {
+			x--
+		}
+		return x
+	}
+	right := func(x, y int) int {
+		for x < maxX && isDigit(lines[y][x]) {
+			x++
+		}
+		return x
+	}
+	readNumbers := func(x1, x2, y int) {
+		// parse number, optionally multiple numbers separated by space
+		var val uint
+		store := func() {
+			numbers[n] = val
+			n++
+			val = 0
+		}
+
+		// trim
+		for x1 < x2 && !isDigit(lines[y][x1]) {
+			x1++
+		}
+		for x1 < x2 && !isDigit(lines[y][x2]) {
+			x2--
+		}
+		// found a number after trimming?
+		if x1 == x2 && !isDigit(lines[y][x1]) {
+			return
+		}
+		for i := x1; i <= x2; i++ {
+			if !isDigit(lines[y][i]) {
+				store()
+			} else {
+				val = 10*val + digit(lines[y][i])
+			}
+		}
+		store()
+	}
+
+	for y := range lines {
+		for x := range maxX {
+			if !isGearSymbol(lines[y][x]) {
+				continue
+			}
+
+			n = 0
+			// North
+			// TODO remove safe, backed min/max combos
+			if y-1 >= 0 {
+				readNumbers(left(max(0, x-1), y-1),
+					right(min(maxX, x+1), y-1), y-1)
+			}
+
+			// West
+			if x-1 >= 0 {
+				readNumbers(left(max(0, x-1), y), max(0, x-1), y)
+			}
+
+			// East
+			if x+1 <= maxX {
+				readNumbers(min(maxX, x+1), right(min(maxX, x+1), y), y)
+			}
+
+			// South
+			if y+1 <= maxY {
+				readNumbers(left(max(0, x-1), y+1),
+					right(min(maxX, x+1), y+1), y+1)
+			}
+
+			sum += gearRatio()
+		}
+	}
+	return
+}
+
+func digit(b byte) uint {
+	return uint(b - '0')
+}
+
 func isDigit(b byte) bool {
 	return b >= '0' && b <= '9'
 }
 
+func isGearSymbol(b byte) bool {
+	return b == '*'
+}
+
+func isSpace(b byte) bool {
+	return b == '.'
+}
+
 func isSymbol(b byte) bool {
-	return !(b == '.' || isDigit(b))
+	return !(isSpace(b) || isDigit(b))
 }
--- day03_test.go
+++ day03_test.go
@@ -13,7 +13,7 @@ func TestDay03Part1Example(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day03(lines)
+	got := Day03Part1(lines)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -27,7 +27,7 @@ func TestDay03Part1(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day03(lines)
+	got := Day03Part1(lines)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
@@ -52,19 +52,55 @@ func BenchmarkDay03Part1(b *testing.B) {
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = Day03(lines)
+		_ = Day03Part1(lines)
+	}
+}
+
+func TestDay03Part2Example(t *testing.T) {
+	const want = 467835
+	lines, err := linesFromFilename(exampleFilename(3))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day03Part2(lines)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay03Part2_116_12(t *testing.T) {
+	const want = 4032
+	got := Day03Part2([]string{
+		".672.",
+		".*...",
+		".6...",
+	})
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
 func TestDay03Part2(t *testing.T) {
-	const want = 539713
+	// const want = 84138748 // too low
+	const want = 84159075
 	lines, err := linesFromFilename(filename(3))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day03(lines)
+	got := Day03Part2(lines)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func BenchmarkDay03Part2(b *testing.B) {
+	lines, err := linesFromFilename(filename(3))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for range b.N {
+		_ = Day03Part2(lines)
 	}
 }
 
