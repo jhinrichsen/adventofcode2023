4a685c493e9d291de292fc39334a0740941c6ee5
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2024-06-18 14:27:11 +0200

    feat: start day 4

--- day04.go
+++ day04.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func Day04(lines []string) (uint, error) {
+func Day04Part1V1(lines []string) (uint, error) {
 	var points uint
 	m := make(map[uint]bool)
 	for _, line := range lines {
@@ -37,4 +37,90 @@ func Day04(lines []string) (uint, error) {
 		points += factor / 2
 	}
 	return points, nil
+}
+
+func Day04(lines []string, part1 bool) (uint, error) {
+	wins, err := NewDay04(lines)
+	if err != nil {
+		return 0, err
+	}
+
+	return day04(wins, part1), nil
+}
+
+func day04(wins []uint, part1 bool) uint {
+	if part1 {
+		var points uint
+		for _, v := range wins {
+			points += A131577(v)
+		}
+		return points
+	}
+
+	var ns = make([]uint, len(wins)) // number of scratchcards
+	for i := range ns {
+		ns[i] = 1 // in the beginning, there is one instance of every card
+	}
+
+	for i, v := range ns {
+		n := wins[i]
+		for j := range n {
+			idx := j + 1
+			ns[idx] += 1
+		}
+	}
+
+	// return total number of scratchcards
+	var n uint
+	for _, v := range ns {
+		n += v
+	}
+	return n
+}
+
+// NewDay04 returns number of winning numbers for card at given index.
+func NewDay04(lines []string) ([]uint, error) {
+	const (
+		base = 10
+		bits = 8 // values in range [1..99]
+	)
+	var cards []uint
+	m := make(map[uint]bool)
+	for _, line := range lines {
+		clear(m)
+		nocards := strings.Split(line, ":")
+		numbers := strings.Split(nocards[1], "|")
+
+		// winning numbers
+		for _, num := range strings.Fields(numbers[0]) {
+			n, err := strconv.ParseUint(num, 10, bits)
+			if err != nil {
+				return nil, err
+			}
+			m[uint(n)] = true
+		}
+
+		// numbers
+		var wins uint
+		for _, num := range strings.Fields(numbers[1]) {
+			n, err := strconv.ParseUint(num, base, bits)
+			if err != nil {
+				return nil, err
+			}
+			if m[uint(n)] {
+				wins++
+			}
+		}
+		cards = append(cards, wins)
+	}
+	return cards, nil
+}
+
+// A131577 returns the OEIS sequence [A131577] 'zero followed by powers of 2'.
+// A131577:[https://oeis.org/A131577]
+func A131577(n uint) uint {
+	if n == 0 {
+		return 0
+	}
+	return 1 << (n - 1)
 }
--- day04_test.go
+++ day04_test.go
@@ -4,13 +4,69 @@ import (
 	"testing"
 )
 
+func TestDay04Part1V1Example(t *testing.T) {
+	const want = 13
+	lines, err := linesFromFilename(exampleFilename(4))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day04Part1V1(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay04Part1V1(t *testing.T) {
+	const want = 26218
+	lines, err := linesFromFilename(filename(4))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day04Part1V1(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func BenchmarkDay04Part1V1(b *testing.B) {
+	lines, err := linesFromFilename(filename(4))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = Day04Part1V1(lines)
+	}
+}
+
+func TestDay04Part2(t *testing.T) {
+	const want = 0
+	lines, err := linesFromFilename(filename(4))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day04Part1V1(lines)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
 func TestDay04Part1Example(t *testing.T) {
 	const want = 13
 	lines, err := linesFromFilename(exampleFilename(4))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got, err := Day04(lines)
+	got, err := Day04(lines, true)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -25,33 +81,7 @@ func TestDay04Part1(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	got, err := Day04(lines)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if want != got {
-		t.Fatalf("want %d but got %d", want, got)
-	}
-}
-
-func BenchmarkDay04Part1(b *testing.B) {
-	lines, err := linesFromFilename(filename(4))
-	if err != nil {
-		b.Fatal(err)
-	}
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_, _ = Day04(lines)
-	}
-}
-
-func TestDay04Part2(t *testing.T) {
-	const want = 0
-	lines, err := linesFromFilename(filename(4))
-	if err != nil {
-		t.Fatal(err)
-	}
-	got, err := Day04(lines)
+	got, err := Day04(lines, true)
 	if err != nil {
 		t.Fatal(err)
 	}
