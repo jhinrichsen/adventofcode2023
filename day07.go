4da99cdcea44bbb229581fc1c54b11672f367e9c
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2024-10-14 19:24:41 +0200

    feat: add day 7 part 2

--- day07.go
+++ day07.go
@@ -13,22 +13,22 @@ type HandType int
 const (
 	HighCard HandType = iota
 	OnePair
-	TwoPair
+	TwoPairs
 	ThreeOfAKind
 	FullHouse
 	FourOfAKind
 	FiveOfAKind
 )
 
-func Day07(hands [][]Card, bids []int) (int, error) {
+func Day07(hands [][]Card, bids []uint, useJoker bool) (uint, error) {
 
 	// sort into ascending order
 
-	ranks := copyDeck(hands)
-	slices.SortFunc(ranks, func(a, b []Card) int {
+	ranked := copyDeck(hands)
+	slices.SortFunc(ranked, func(a, b []Card) int {
 
-		n1 := handType(a)
-		n2 := handType(b)
+		n1 := handType(a, useJoker)
+		n2 := handType(b, useJoker)
 		if n1 != n2 {
 			return int(n1) - int(n2)
 		}
@@ -40,15 +40,16 @@ func Day07(hands [][]Card, bids []int) (int, error) {
 			} else if a[i] > b[i] {
 				return 1
 			}
+			// equal, continue with next card
 		}
 		return 0
 	})
 
-	var total int
-	for i, r := range ranks {
-		rank := i + 1 // one-based
+	var total uint
+	for i, r := range ranked {
+		rank := 1 + uint(i) // one-based
 		// go back into original hands to find bid
-		var bid int
+		var bid uint
 		for j, h := range hands {
 			if slices.Compare(r, h) == 0 {
 				bid = bids[j]
@@ -69,10 +70,10 @@ func copyDeck(matrix [][]Card) [][]Card {
 	return duplicate
 }
 
-func NewDay07(lines []string) ([][]Card, []int, error) {
+func NewDay07(lines []string, part1 bool) ([][]Card, []uint, error) {
 	var (
 		hands [][]Card
-		bids  []int
+		bids  []uint
 	)
 	for i, line := range lines {
 		parts := strings.Fields(line)
@@ -84,7 +85,7 @@ func NewDay07(lines []string) ([][]Card, []int, error) {
 
 		var hand []Card
 		for j, b := range parts[0] {
-			c, err := card(byte(b))
+			c, err := card(byte(b), part1)
 			if err != nil {
 				return nil, nil, fmt.Errorf("line %d, card %d: %c is not a valid card", i, j, b)
 			}
@@ -98,47 +99,98 @@ func NewDay07(lines []string) ([][]Card, []int, error) {
 		if err != nil {
 			return nil, nil, fmt.Errorf("line %d: want numeric column 2 but got %s: %v", i, parts[1], err)
 		}
-		bids = append(bids, n)
+		bids = append(bids, uint(n))
 	}
 	return hands, bids, nil
 }
 
-func handType(hand []Card) HandType {
+func handType(hand []Card, useJoker bool) HandType {
+
+	// sort by count ascending
+
 	const N = 13
 	var cnt [N]int
 	for _, b := range hand {
+		if useJoker { // consider joker?
+			if b == 0 { // joker do not count for hand type
+				continue
+			}
+		}
 		cnt[b]++
 	}
 	slices.Sort(cnt[:])
 
 	h1, h2 := cnt[N-1], cnt[N-2]
+	var t HandType
 	if h1 == 5 {
-		return FiveOfAKind
-	}
-	if h1 == 4 {
-		return FourOfAKind
-	}
-	if h1 == 3 {
+		t = FiveOfAKind
+	} else if h1 == 4 {
+		t = FourOfAKind
+	} else if h1 == 3 {
 		if h2 == 2 {
-			return FullHouse
+			t = FullHouse
+		} else {
+			t = ThreeOfAKind
 		}
-		return ThreeOfAKind
-	}
-	if h1 == 2 {
+	} else if h1 == 2 {
 		if h2 == 2 {
-			return TwoPair
+			t = TwoPairs
+		} else {
+			t = OnePair
 		}
-		return OnePair
+	} else {
+		t = HighCard
 	}
-	return HighCard
+
+	if !useJoker {
+		return t
+	}
+
+	n := joker(hand)
+	for i := 0; i < n; i++ {
+		switch t {
+		case FiveOfAKind:
+			t = FiveOfAKind
+		case FourOfAKind:
+			t = FiveOfAKind
+		case FullHouse:
+			t = FourOfAKind
+		case ThreeOfAKind:
+			t = FourOfAKind
+		case TwoPairs:
+			t = FullHouse
+		case OnePair:
+			t = ThreeOfAKind
+		case HighCard:
+			t = OnePair
+		default:
+			panic("incomplete switch case")
+		}
+	}
+	return t
 }
 
-func card(b byte) (Card, error) {
-	var cards = []byte{'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'}
+func card(b byte, joker bool) (Card, error) {
+	var cards []byte
+	if joker {
+		cards = []byte{'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'}
+	} else {
+		cards = []byte{'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'}
+	}
 	for i, v := range cards {
 		if b == v {
 			return Card(i), nil
 		}
 	}
 	return Card(0), fmt.Errorf("Unknown card: want one of %+v but got %c", cards, b)
+}
+
+// joker returns number of Joker in hand.
+func joker(hand []Card) (n int) {
+	for _, h := range hand {
+		if h == 0 {
+			n++
+		}
+	}
+	return
 }
--- day07_test.go
+++ day07_test.go
@@ -5,16 +5,19 @@ import (
 )
 
 func TestDay07Part1Example(t *testing.T) {
-	const want = 6440
+	const (
+		joker = false
+		want  = 6440
+	)
 	lines, err := linesFromFilename(exampleFilename(07))
 	if err != nil {
 		t.Fatal(err)
 	}
-	hands, bids, err := NewDay07(lines)
+	hands, bids, err := NewDay07(lines, joker)
 	if err != nil {
 		t.Fatal(err)
 	}
-	got, err := Day07(hands, bids)
+	got, err := Day07(hands, bids, joker)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -24,16 +27,19 @@ func TestDay07Part1Example(t *testing.T) {
 }
 
 func TestDay07Part1(t *testing.T) {
-	const want = 246424613
+	const (
+		joker = false
+		want  = 246424613
+	)
 	lines, err := linesFromFilename(filename(07))
 	if err != nil {
 		t.Fatal(err)
 	}
-	hands, bids, err := NewDay07(lines)
+	hands, bids, err := NewDay07(lines, joker)
 	if err != nil {
 		t.Fatal(err)
 	}
-	got, err := Day07(hands, bids)
+	got, err := Day07(hands, bids, joker)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -43,41 +49,139 @@ func TestDay07Part1(t *testing.T) {
 }
 
 func BenchmarkDay07Part1(b *testing.B) {
+	const joker = false
 	lines, err := linesFromFilename(filename(07))
 	if err != nil {
 		b.Fatal(err)
 	}
-	hands, bids, err := NewDay07(lines)
+	hands, bids, err := NewDay07(lines, joker)
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = Day07(hands, bids)
+		_, _ = Day07(hands, bids, joker)
 	}
 }
 
-/*
-func TestDay07Part2(t *testing.T) {
+func TestCard(t *testing.T) {
 	const want = 0
+	var mu bool
+	got, err := card('2', mu)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay07Part2Example(t *testing.T) {
+	const (
+		joker = true
+		want  = 5905
+	)
+	lines, err := linesFromFilename(exampleFilename(07))
+	if err != nil {
+		t.Fatal(err)
+	}
+	hands, bids, err := NewDay07(lines, joker)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day07(hands, bids, joker)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay07Part2Example2(t *testing.T) {
+	const (
+		joker = true
+		want  = 6839
+	)
+	lines, err := linesFromFilename("testdata/day07_example2.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	hands, bids, err := NewDay07(lines, joker)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day07(hands, bids, joker)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay07Part2(t *testing.T) {
+	const (
+		joker = true
+		// want  = 248822253 too high
+		want = 248256639
+	)
 	lines, err := linesFromFilename(filename(07))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day07(lines)
-	if want != got {
-		t.Fatalf("want %d but got %d", want, got)
+	hands, bids, err := NewDay07(lines, joker)
+	if err != nil {
+		t.Fatal(err)
 	}
-}
-*/
-
-func TestCard(t *testing.T) {
-	const want = 0
-	got, err := card('2')
+	got, err := Day07(hands, bids, joker)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestQ2KJJ(t *testing.T) {
+	const (
+		want = ThreeOfAKind
+		s    = "Q2KJJ"
+	)
+	hand, err := cards(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := handType(hand, true)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func cards(s string) ([]Card, error) {
+	var hand = make([]Card, len(s))
+	for i, b := range s {
+		c, err := card(byte(b), true)
+		if err != nil {
+			return nil, err
+		}
+		hand[i] = c
+	}
+	return hand, nil
+}
+
+func BenchmarkDay07Part2(b *testing.B) {
+	const joker = true
+	lines, err := linesFromFilename(filename(07))
+	if err != nil {
+		b.Fatal(err)
+	}
+	hands, bids, err := NewDay07(lines, joker)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = Day07(hands, bids, joker)
 	}
 }
